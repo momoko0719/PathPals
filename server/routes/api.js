6113450226f73608785ae922f0bff2aa5a954001
SamRitian <xyou@uw.edu>
2023-11-19 21:30:10 -0800

    partially imlemented Create feature with Google's Places API

--- server/routes/api.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var placesRouter = require('./api/places');
-
-const router = express.Router();
-
-router.use("/places", placesRouter);
-
-module.exports = router;
