9cc41d44c3a1119e76303df0a555384ed217c7d5
Allison-Ho <qnho@uw.edu>
2023-11-21 09:10:48 -0800

    add code for fetching from Google API

---
+++ server/routes/api/addPlaces.js
@@ -0,0 +1,31 @@
+const express =  require('express');
+const {Client} = require('@googlemaps/google-maps-services-js');
+
+const router = express.Router();
+
+// create a new client to make the fetch call
+const client = new Client({});
+
+// given a text input of a place, return the information of that place
+router.get('/', async (req, res) => {
+  try{
+    if(req.body.id){
+      // use placeId to make another request to get places details
+      const params = {
+        place_id: id,
+        fields: ['formatted_address', 'name', 'photos'],
+        key: process.env.GOOGLE_MAPS_API_KEY
+      }
+
+      let response = await client.placeDetails({params: params});
+
+      res.json(response.data.result);
+    }else{
+      res.type('text').send('Please input a place name');
+    }
+  }catch(err){
+    console.log(err);
+  }
+});
+
+module.exports = router;
