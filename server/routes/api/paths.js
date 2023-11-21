f9e9849cacab5d8ed635461fa8133cfccaef09d9
momoko0719 <yifeit2@uw.edu>
2023-11-20 17:39:28 -0800

    allows users to search by keywords in path_name or places name

---
+++ server/routes/api/paths.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var models = require("../../models");
+var router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  const paths = await models.Itinerary.find();
+  let pathsData = await Promise.all(
+    paths.map(async (path) => {
+      try {
+        return {
+          username: path.username,
+          path_name: path.path_name,
+          description: path.description,
+          places: path.places,
+          date_created: path.date_created,
+          num_views: path.num_views,
+          likes: path.likes, // an array of usernames that liked this itinerary
+          shared: path.shared,
+        };
+      } catch (error) {
+        res.status(500).json({ status: "error", error: error });
+      }
+    })
+  );
+  res.json(pathsData);
+});
+
+module.exports = router;
