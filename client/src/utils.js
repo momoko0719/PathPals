75a599bb33b0a6a8228c03b59834136b41760130
Allison-Ho <qnho@uw.edu>
2023-12-06 23:26:50 -0800

    please let this be the last bug

---
+++ client/src/utils.js
@@ -0,0 +1,7 @@
+function errorHandling(errorMsg) {
+  document.getElementById("error").style.display = "hidden";
+
+  setTimeout(() => {
+    document.getElementById("error").style.display = "hidden";
+  }, 5000);
+}
